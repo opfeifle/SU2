8ac4247792ddfde3d4f6ce46d4f4efb2e4ac4a5f
opfeifle <ole.pfeifle@gmail.com>
2017-05-12 19:49:44 +0200

    error_fix

--- SU2_CFD/src/solver_direct_mean.cpp
+++ SU2_CFD/src/solver_direct_mean.cpp
@@ -11894,7 +11894,7 @@ void CEulerSolver::BC_Inlet(CGeometry *geometry, CSolver **solver_container,
 
     iPoint = geometry->vertex[val_marker][iVertex]->GetNode();
 
-    CartCoord = geometry->vertex[val_marker][iVertex]->GetCoord();
+    CartCoord = geometry->vertex[val_marker][iVertex]->GetCoord(1);
 
 
     /*--- Check if the node belongs to the domain (i.e., not a halo node) ---*/
