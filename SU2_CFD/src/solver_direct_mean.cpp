76b234a435f2825957072b394ccb1f10f3153327
opfeifle <ole.pfeifle@gmail.com>
2017-05-12 18:21:36 +0200

    error_fix

--- SU2_CFD/src/solver_direct_mean.cpp
+++ SU2_CFD/src/solver_direct_mean.cpp
@@ -11956,12 +11956,12 @@ void CEulerSolver::BC_Inlet(CGeometry *geometry, CSolver **solver_container,
           } */
 
           alphaT = 110.0;
-          if (nDim == 3) {
-            Flow_Dir[0] =  cos((alpha-90)/57.3); /* local swirl [rad] = (alphaT-90)/57.3   */
+          /* if (nDim == 3) {
+            Flow_Dir[0] =  cos((alpha-90)/57.3); */ /* local swirl [rad] = (alphaT-90)/57.3
             Flow_Dir[1] = -CartCoord[2] * sin((alpha-90)/57.3) / CoordRad;
             Flow_Dir[2] =  CartCoord[1] * sin((alpha-90)/57.3) / CoordRad;
 
-          };
+          }; */
 
           /*--- Non-dim. the inputs if necessary. ---*/
 
